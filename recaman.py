d9d9a56f29fe8497a1418cd0e303d4547bc28a91
mikey0830 <lpgb8515@leeds.ac.uk>
2024-10-25 00:23:55 +0000

    Add recaman.py file

--- recaman.py
+++ recaman.py
@@ -1,10 +1,9 @@
 def recaman_sequence(n):
     sequence = [0]  
-    seen = {0}  
+    seen = {0}      
     for k in range(1, n):
-        previous = sequence[-1]
-        next_num = previous - k
-        
+        previous = sequence[-1]  
+        next_num = previous - k  
         if next_num > 0 and next_num not in seen:
             sequence.append(next_num)
             seen.add(next_num)
@@ -14,6 +13,19 @@ def recaman_sequence(n):
             seen.add(next_num)
     
     return sequence
+
 if __name__ == "__main__":
-    sequence = recaman_sequence(32)  
-    print(sequence)
+    sequence = recaman_sequence(1)
+    print(sequence)  
+
+    sequence = recaman_sequence(4)
+    print(sequence)  
+
+    sequence = recaman_sequence(8)
+    print(sequence)  
+
+    sequence = recaman_sequence(16)
+    print(sequence)  
+
+    sequence = recaman_sequence(32)
+    print(sequence)  
